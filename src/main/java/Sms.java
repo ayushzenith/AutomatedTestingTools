9a634aa65ed5707a3a29dc0c0e67eba9e5a1c159
Ayush Zenith <ayushp.zenith@gmail.com>
2020-08-21 19:10:51 -0400

    Update Sms.java

--- src/main/java/Sms.java
+++ src/main/java/Sms.java
@@ -1,6 +1,7 @@
 import com.twilio.Twilio;
 import com.twilio.base.ResourceSet;
 import com.twilio.rest.api.v2010.account.Message;
+import org.joda.time.DateTime;
 
 import java.util.ArrayList;
 import java.util.regex.Matcher;
@@ -30,19 +31,26 @@ public class Sms {
         this.AUTH_TOKEN = AUTH_TOKEN;
     }
 
-    public String fetch(String findSender, String orderNumber, int numberOfTexts) {
+    public String fetch(String findSender, String keyword, int numberOfTexts, DateTime date) {
+        this.body = "";
+        this.sender = "";
+        this.message = "";
         Twilio.init(this.ACCOUNT_SID, this.AUTH_TOKEN);
         ResourceSet<Message> messages = Message.reader().limit(numberOfTexts).read();
         for(Message record : messages) {
-            if (record.getFrom().toString().contains(findSender) && record.getBody().contains(orderNumber)) {
-                this.body = record.getBody().toString();
-                this.sender = record.getFrom().toString();
-                this.message = "From: " + record.getFrom().toString() + "\nTo: " + record.getTo().toString() + "\nDate Sent: " + record.getDateSent().toString() + "\nBody: " + record.getBody().toString();
-                return message;
+            if (record.getFrom().toString().contains(findSender) && record.getBody().contains(keyword)) {
+                if (record.getDateSent().isAfter(date) || record.getDateSent().isEqual(date)) {
+                    this.body = record.getBody().toString();
+                    this.sender = record.getFrom().toString();
+                    this.message = "From: " + record.getFrom().toString() + "\nTo: " + record.getTo().toString() + "\nDate Sent: " + record.getDateSent().toString() + "\nBody: " + record.getBody().toString();
+                } else {
+                    return message;
+                }
             }
         }
         return "";
     }
+    
     public ArrayList getLinks(String baseUrl) {
         return getLinks(baseUrl, getBody());
     }
@@ -73,14 +81,19 @@ public class Sms {
 
         // Create object
         Sms sms = new Sms(ACCOUNT_SID, AUTH_TOKEN);
+        
+        //20-8-2020 00:00:00
+        DateTime date = new DateTime(2020, 8, 20, 0, 0, 0);
 
-        // .fetch("sender", "orderNo", 12) will search for an email in which the sender number CONTAINS Argument 1, the sender IS Argument 2, and Argument 3 determines how many texts should be searched starting at the latest text.
+        // .fetch("senderNo.", "Keyword", NumberOfTextsToSearch, DateTime)
         // Arguments are case sensitive
         // The function returns the full message as a String
         // .fetch() needs to be called to initialize .getSender(), .getBody(), .getMessage(), .getLinks(), etc
         // Everytime .fetch() is called all the other information will be reinitialized according to the search parameters given
-        // Fetch starts searching from the most recent message in the twilio message inbox and outbox and once it finds an email that meets all of the search criterion it stop the search
-        String text = sms.fetch("+10123456789", "NXTUPLE-123456789-21",1000000000);
+        // Fetch starts searching from the most recent message in the twilio message inbox and outbox and once it finds a text that meets all of the search criterion it stop the search
+        // In this it will find a text that came from the number "+10123456789" and the text must have the word "pickup" in it. It will search up to 1000000000 texts before terminating and if it finds multiple texts that fit the critereon then it will find the one that was sent right after 20-8-2020 00:00:00. It doesnt take any text before that date into consideration at all.
+
+        String text = sms.fetch("+10123456789", "pickup",1000000000, date);
 
         // .getSender() returns the sender phone number as a String
         System.out.println("Sender: " + sms.getSender() + "\n");
